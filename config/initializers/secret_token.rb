18532dd6bcb9f38a0078cedcad4f155128f29b52
Kendrick Coleman <kendrickcoleman@gmail.com>
2013-11-20 11:31:07 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vblockpretty::Application.config.secret_key_base = 'dcf5f691b4a21cc06aa9dd49530deb5c6d5436edeadac5452e70d379c92aa1902403807ad5f13fe53032e6f5f6c0276f1b3c77a44fe7a0a869e8f7f16810abc9'
